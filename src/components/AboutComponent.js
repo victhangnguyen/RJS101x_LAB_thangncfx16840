d0f218011c5ac3d68e7a4caa1c5f6bb93e59df14
victhangnguyen <victhangnguyen@outlook.com>
2022-07-16 22:57:08 +0700

    Controlled Forms

--- src/components/AboutComponent.js
+++ src/components/AboutComponent.js
@@ -9,20 +9,20 @@ import {
 } from 'reactstrap';
 import { Link } from 'react-router-dom';
 
-function RenderLeader({ leader }) {
-  return (
-    <Media tag="li">
-      <Media left middle>
-        <Media object src={leader.image} alt={leader.name} />
+  function RenderLeader({ leader }) {
+    return (
+      <Media tag="li">
+        <Media left middle>
+          <Media object src={leader.image} alt={leader.name} />
+        </Media>
+        <Media body className="ml-5">
+          <Media heading>{leader.name}</Media>
+          <p>{leader.designation}</p>
+          <p>{leader.description}</p>
+        </Media>
       </Media>
-      <Media body className="ml-5">
-        <Media heading>{leader.name}</Media>
-        <p>{leader.designation}</p>
-        <p>{leader.description}</p>
-      </Media>
-    </Media>
-  );
-}
+    );
+  }
 
 function About(props) {
   const leaders = props.leaders.map((leader) => {
